e532429c725864c997e407fcbc5f250cb6e45022
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-02 22:24:24 +0100

    fetch shots

--- MMM-dribbble.js
+++ MMM-dribbble.js
@@ -1,5 +1,12 @@
 Module.register("MMM-dribbble", {
 	defaults: {
+		api: {
+			base: "https://api.dribbble.com/v1",
+			clientAccessToken: null,
+			endpoint: "shots",
+			perPage: 24,
+		},
+		animationSpeed: 1000,
 		initialLoadDelay: 1000 * 2.5,
 		updateInterval: 1000 * 60 * 10
 	},
@@ -16,8 +23,25 @@ Module.register("MMM-dribbble", {
 		return wrapper;
 	},
 
-	update: function() {
+	getShots: function() {
+		return new Promise((resolve, reject) => {
+			const { api } = this.config;
+			const url = `${api.base}/${api.endpoint}?access_token=${api.clientAccessToken}&per_page=${api.perPage}`;
 
+			fetch(url)
+				.then(response => response.json())
+				.then(result => resolve(result))
+				.catch(error => reject(error));
+		});
+	},
+
+	update: function() {
+		this.getShots().then(shots => {
+			this.shots = shots;
+			this.show(this.config.animationSpeed, { lockString: this.identifier} );
+			this.loaded = true;
+			this.updateDom(this.config.animationSpeed);
+		});
 	},
 
 	scheduleUpdate: function(delay) {
