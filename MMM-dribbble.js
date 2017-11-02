b502c264f90cc42afd2b0c1f1cffea7712f11bd3
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-02 22:22:44 +0100

    add scheduleUpdate method

--- MMM-dribbble.js
+++ MMM-dribbble.js
@@ -1,6 +1,7 @@
 Module.register("MMM-dribbble", {
 	defaults: {
-
+		initialLoadDelay: 1000 * 2.5,
+		updateInterval: 1000 * 60 * 10
 	},
 
 	getStyles: function() {
@@ -15,7 +16,29 @@ Module.register("MMM-dribbble", {
 		return wrapper;
 	},
 
+	update: function() {
+
+	},
+
+	scheduleUpdate: function(delay) {
+		let nextLoad = this.config.updateInterval;
+
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+
+		clearTimeout(this.updateTimer);
+		this.updateTimer = setTimeout(() => {
+			this.update();
+		}, nextLoad);
+	},
+
 	start: function() {
 		Log.info(`Starting module: ${this.name}`);
+
+		this.loaded = false;
+		this.scheduleUpdate(this.config.initialLoadDelay);
+
+		this.updateTimer = null;
 	}
 });
