897bb56fdf73b8f2d528fb0519585f01d520b390
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-02 22:06:39 +0100

    setup module

---
+++ MMM-dribbble.js
@@ -0,0 +1,21 @@
+Module.register("MMM-dribbble", {
+	defaults: {
+		
+	},
+
+	getStyles: function() {
+		return [
+			"MMM-dribbble.css",
+		];
+	},
+
+	getDom: function() {
+		const wrapper = document.createElement("div");
+
+		return wrapper;
+	},
+
+	start: function() {
+		Log.info(`Starting module: ${this.name}`);
+	}
+});
